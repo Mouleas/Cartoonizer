cc96e1b9b28a337740911e0f13c0bc854d33e45d
Mouleaswar <74062509+M0u1ea5@users.noreply.github.com>
2020-12-10 20:12:53 +0530

    Add files via upload

---
+++ cartoonizer/cartoonizer.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img=cv2.imread(r'C:\Users\M0u1ea5\Pictures\dog.jpg')
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray,5)
+edges = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,9,9)
+
+color = cv2.bilateralFilter(img,9,250,250)
+cartoon = cv2.bitwise_and(color,color,mask=edges)
+
+cv2.imshow('image',img)
+cv2.imshow('edges',edges)
+cv2.imshow('cartoon',cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
